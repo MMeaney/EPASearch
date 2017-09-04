c3561684385310d4237d875e798fb45e25c36217
Maurice Meaney <maurice.meaney@gmail.com>
2017-09-04 15:41:33 +0100

    Update run.py

--- api/eve/aq_ozone_elastic/run.py
+++ api/eve/aq_ozone_elastic/run.py
@@ -1,7 +1,36 @@
 import eve
 from eve_elastic import Elastic
 
+#### Added for Swagger
+from eve_swagger import swagger, add_documentation
+#### End
+
 app = eve.Eve(data=Elastic)
+
+
+#### Added for Swagger
+app.register_blueprint(swagger)
+
+# required. See http://swagger.io/specification/#infoObject for details.
+app.config['SWAGGER_INFO'] = {
+    'title': 'My Supercool API',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'nicola',
+        'url': 'http://nicolaiarocci.com'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/pyeve/eve-swagger/blob/master/LICENSE',
+    },
+    'schemes': ['http', 'https'],
+}
+
+#### End
+
+
 app.run(host='0.0.0.0', port=5014)
 
 #from eve import Eve
